7700fe71ac0a35651b8b822bb6b4a4c8c511436d
ASHAN2021 <akh8130@gmail.com>
2025-02-23 23:17:30 +0530

    gettoken and get username using jwt token

--- jwtdeveloptutorials/src/main/java/com/ashandevelopment/jwtdeveloptutorials/controller/HomeController.java
+++ jwtdeveloptutorials/src/main/java/com/ashandevelopment/jwtdeveloptutorials/controller/HomeController.java
@@ -1,19 +1,34 @@
 package com.ashandevelopment.jwtdeveloptutorials.controller;
 
+import com.ashandevelopment.jwtdeveloptutorials.service.JWTService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final JWTService jwtService;
+
+    public HomeController(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+
     @GetMapping
     public String getHello() {
+
         return "Hello World";
     }
 
     @PostMapping("/login")
     public String login(){
-        return "user logged in";
+        return jwtService.getJWTToken();
+    }
+
+    @GetMapping("/username")
+    public String getUsername(@RequestParam String token){
+        return jwtService.getUsername(token);
     }
 }
