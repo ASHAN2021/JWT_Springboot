82446504ec42a61c958d2c2c17f07886ccbd203e
ASHAN2021 <akh8130@gmail.com>
2025-02-27 17:40:08 +0530

    user details service and password encoder

--- jwtdeveloptutorials/src/main/java/com/ashandevelopment/jwtdeveloptutorials/config/SecurityConfig.java
+++ jwtdeveloptutorials/src/main/java/com/ashandevelopment/jwtdeveloptutorials/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.ashandevelopment.jwtdeveloptutorials.config;
 
+import com.ashandevelopment.jwtdeveloptutorials.service.MyUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+//    private final MyUserDetailsService myUserDetailsService;
+//
+//    public SecurityConfig(MyUserDetailsService myUserDetailsService) {
+//        this.myUserDetailsService = myUserDetailsService;
+//    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -20,7 +31,26 @@ public class SecurityConfig {
         .authorizeHttpRequests(r->r.requestMatchers("/login")
                 .permitAll().anyRequest()
                 .authenticated())
+                .authenticationProvider(authenticationProvider())
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public MyUserDetailsService userDetailsService() {
+        return new MyUserDetailsService(passwordEncoder());
     }
 }
--- jwtdeveloptutorials/src/main/java/com/ashandevelopment/jwtdeveloptutorials/service/JWTService.java
+++ jwtdeveloptutorials/src/main/java/com/ashandevelopment/jwtdeveloptutorials/service/JWTService.java
@@ -33,10 +33,15 @@ public class JWTService {
     }
 
     public  String getUsername(String token) {
-        return Jwts
-                .parser()
-                .verifyWith(secretKey)
-                .build()
-                .parseSignedClaims(token).getPayload().getSubject();
+        try {
+            return Jwts
+                    .parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token).getPayload().getSubject();
+
+        } catch (Exception e) {
+            return "invalid token";
+        }
     }
 }
